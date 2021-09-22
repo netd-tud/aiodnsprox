31328926ee081c995f90e630ff181372473b5242
Martine Lenders <m.lenders@fu-berlin.de>
2021-09-22 17:30:19 +0200

    coap: document module

--- aiodnsprox/coap.py
+++ aiodnsprox/coap.py
@@ -5,6 +5,8 @@
 #
 # Distributed under terms of the MIT license.
 
+"""DNS over CoAP serving side of the proxy."""
+
 import asyncio
 import base64
 
@@ -21,12 +23,19 @@ CONTENT_FORMAT_DNS_MESSAGE = 65053
 
 
 class NotAcceptable(aiocoap.error.ConstructionRenderableError):
+    """Exception to represent a 4.06 Not Acceptable CoAP error response"""
     code = aiocoap.numbers.codes.Code.NOT_ACCEPTABLE
 
 
 class DNSOverCoAPServerFactory(BaseServerFactory):
+    """Factory to create DNS over CoAP servers"""
     # pylint: disable=too-few-public-methods
     class DNSQueryResource(aiocoap.resource.Resource, DNSUpstreamServerMixin):
+        """The DNS over CoAP resource of the DNS over CoAP server.
+
+        :param factory: The factory that created the DNS over CoAP server.
+        :type factory: :py:class:`DNSOverCoAPServerFactory`
+        """
         def __init__(self, factory):
             super().__init__(host=factory.upstream_host,
                              port=factory.upstream_port,
@@ -68,9 +77,21 @@ class DNSOverCoAPServerFactory(BaseServerFactory):
             return await self._render_acceptable(request, query)
 
         async def render_fetch(self, request):
+            """:py:class:`aiocoap.resource.Resource` method to serve a FETCH
+            request
+
+            :param request: The FETCH request
+            :returns: The response for the FETCH request.
+            """
             return await self._render_with_payload(request)
 
         async def render_get(self, request):
+            """:py:class:`aiocoap.resource.Resource` method to serve a GET
+            request
+
+            :param request: The GET request
+            :returns: The response for the GET request.
+            """
             queries = dict(q.split("=") for q in request.opt.uri_query)
             if 'dns' in queries:
                 query_code = queries['dns']
@@ -83,9 +104,18 @@ class DNSOverCoAPServerFactory(BaseServerFactory):
             raise aiocoap.error.BadRequest()
 
         async def render_post(self, request):
+            """:py:class:`aiocoap.resource.Resource` method to serve a POST
+            request
+
+            :param request: The POST request
+            :returns: The response for the POST request.
+            """
             return await self._render_with_payload(request)
 
     class ClosableContext(aiocoap.Context, BaseDNSServer):
+        """:py:class:`aiocoap.Context` that also serves as an extension of
+        :py:class:`.dns_server.BaseDNSServer` so it can be returned by
+        :py:meth:`DNSOverCoAPServerFactory.create_server`"""
         async def close(self):
             ri_type = type(self.request_interfaces)
             if self.request_interfaces:
@@ -96,7 +126,7 @@ class DNSOverCoAPServerFactory(BaseServerFactory):
                     pass
                 self.request_interfaces = ri_type()
 
-    class CredentialStore:
+    class _CredentialStore:
         # hacking our own SecretStore, so that config is observed
         def __init__(self):
             try:
@@ -113,10 +143,23 @@ class DNSOverCoAPServerFactory(BaseServerFactory):
         def __getitem__(self, key):
             return self._dict[key]
 
-        def keys(self):
+        def keys(self):     # pylint: disable=missing-function-docstring
             return self._dict.keys()
 
     async def create_server(self, loop, *args, local_addr=None, **kwargs):
+        """Creates an ``aiocoap`` server context.
+
+        :param loop: the asyncio event loop the server should run in
+        :type loop: :py:class:`asyncio.AbstractEventLoop`
+        :param local_addr: A tuple for the created server to bind to. The first
+                           element is the host part, the second element the
+                           port.
+        :type local_addr: :py:class:`typing.Tuple[str, int]`
+
+        :returns: An :py:class:`ClosableContext` object representing an
+                  ``aiocoap`` server context.
+        :rtype: :py:class:`ClosableContext`
+        """
         if local_addr is None:
             local_addr = ('localhost', None)
         site = aiocoap.resource.Site()
@@ -127,6 +170,6 @@ class DNSOverCoAPServerFactory(BaseServerFactory):
         site.add_resource(['dns-query'], self.DNSQueryResource(self))
 
         aiocoap.transports.tinydtls_server.securitystore = \
-            self.CredentialStore()
+            self._CredentialStore()
         return await self.ClosableContext.create_server_context(site,
                                                                 local_addr)
