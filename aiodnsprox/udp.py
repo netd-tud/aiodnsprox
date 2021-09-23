26af7e495e7807f3ec0060267c2bfeff84e924c8
Martine Lenders <m.lenders@fu-berlin.de>
2021-09-23 09:26:12 +0200

    udp: document module

--- aiodnsprox/udp.py
+++ aiodnsprox/udp.py
@@ -5,15 +5,23 @@
 #
 # Distributed under terms of the MIT license.
 
+"""DNS over UDP serving side of the proxy."""
+
 from .dns_server import BaseServerFactory, BaseDNSServer
 from .dns_upstream import DNSUpstreamServerMixin
 
 
 class DNSOverUDPServerFactory(BaseServerFactory):
+    """Factory to create DNS over UDP servers"""
     # pylint: disable=too-few-public-methods
     DNS_PORT = 53
 
     class DNSOverUDPServer(BaseDNSServer, DNSUpstreamServerMixin):
+        """DNS over UDP server implementation
+
+        :param factory: The factory that created the DNS over DTLS server.
+        :type factory: :py:class:`DNSOverUDPServerFactory`
+        """
         def __init__(self, factory):
             super().__init__(host=factory.upstream_host,
                              port=factory.upstream_port,
@@ -25,15 +33,35 @@ class DNSOverUDPServerFactory(BaseServerFactory):
             self.transport.sendto(response, requester)
 
         def connection_made(self, transport):
+            # pylint: disable=line-too-long
+            """See `connection_made()`_
+
+            .. _`connection_made()`: https://docs.python.org/3/library/asyncio-protocol.html#asyncio.BaseProtocol.connection_made
+            """     # noqa: E501
             self.transport = transport
 
         def datagram_received(self, data, addr):
+            # pylint: disable=line-too-long
+            """See `datagram_received()`_
+
+            .. _`datagram_received()`: https://docs.python.org/3/library/asyncio-protocol.html#asyncio.DatagramProtocol.datagram_received
+            """     # noqa: E501
             self.dns_query_received(data, addr)
 
         def error_received(self, exc):      # pylint: disable=no-self-use
+            # pylint: disable=line-too-long
+            """See `error_received()`_
+
+            .. _`error_received()`: https://docs.python.org/3/library/asyncio-protocol.html#asyncio.DatagramProtocol.error_received
+            """     # noqa: E501
             raise exc                       # pragma: no cover
 
         def connection_lost(self, exc):     # pylint: disable=no-self-use
+            # pylint: disable=line-too-long,unnecessary-pass
+            """See `connection_lost()`_
+
+            .. _`connection_lost()`: https://docs.python.org/3/library/asyncio-protocol.html#asyncio.BaseProtocol.connection_lost
+            """     # noqa: E501
             pass                            # pragma: no cover
 
         async def close(self):
@@ -46,6 +74,19 @@ class DNSOverUDPServerFactory(BaseServerFactory):
 
     async def create_server(self, loop, *args, local_addr=None,
                             **kwargs):
+        """Creates an :py:class:`DNSOverUDPServer` object.
+
+        :param loop: the asyncio event loop the server should run in
+        :type loop: :py:class:`asyncio.AbstractEventLoop`
+        :param local_addr: A tuple for the created server to bind to. The first
+                           element is the host part, the second element the
+                           port.
+        :type local_addr: :py:class:`typing.Tuple[str, int]`
+
+        :returns: An :py:class:`DNSOverUDPServer` object representing an
+                  DNS over DTLS server.
+        :rtype: :py:class:`DNSOverUDPServer`
+        """
         if local_addr is None:
             local_addr = ('localhost', self.DNS_PORT)
         if local_addr[1] is None:
